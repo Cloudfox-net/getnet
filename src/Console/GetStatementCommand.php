ca3bbb668c02d0d57a277fc318276a6aa198938a
Rupert Lustosa <rupertlustosa@gmail.com>
2021-02-22 09:53:42 -0300

    Clean code

--- src/Console/GetStatementCommand.php
+++ src/Console/GetStatementCommand.php
@@ -40,36 +40,6 @@ class GetStatementCommand extends Command
     public function handle()
     {
         
-        $companies = [
-            22,
-            48,
-            67,
-            85,
-            129,
-            131,
-            132,
-            155,
-            233,
-            441,
-            471,
-            476,
-            596,
-            718,
-            1240,
-            1521,
-            1641,
-            1811,
-            1850,
-            1902,
-            1946,
-            1967,
-            1989,
-            2104,
-            2116,
-            2131,
-            2134,
-            2139
-        ];
         /*$companyId = $this->ask('Qual a company_id?', 3035);
         
         $company = DB::table('companies')
